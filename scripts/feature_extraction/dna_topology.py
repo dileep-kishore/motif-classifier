abe63b2e6da7b46817e85f066a88a22180658d61
Manuel Giménez <manugimenez@gmail.com>
2016-12-03 18:15:32 -0500

    Add feature extractor

--- scripts/feature_extraction/dna_topology.py
+++ scripts/feature_extraction/dna_topology.py
@@ -3,15 +3,64 @@
 """
 Created on Fri Dec  2 16:53:04 2016
 
-@author: Manu
+@author: Manuel Gimenez
 """
 
 import pandas as pd
+from functools import partial
 
-# FIX THIS! It should be read from the package
-features_data_path = "../../data/features/"
+def load_from_topology_db(complete_path_to_file):
+    column_names = ['wtf', 'from', 'to', 'value']
+    cols_to_use = ['to', 'value']
+    data_df = pd.read_csv(complete_path_to_file, sep='\t', comment='#', skip_blank_lines=True, names=column_names, usecols=cols_to_use, skiprows=1)
+    return data_df
 
-# The name of the file with the RegulonDB data of TFs and their binding site
-regulondb_tfbs_filename = features_data_path + "regulondb-tfbs.txt"
+def get_DNA_topology_value(the_data, index_template, a_series):
+    start = a_series[0]
+    end = a_series[1]
+    data_filtered = the_data[ (the_data['to'] >= start) & (the_data['to'] <= end)]
+    res = data_filtered.iloc[:,1]
+    labels = [index_template + str(x) for x in xrange(end-start+1) ]
+    return pd.Series(res.tolist(), index=labels)
 
-column_names = ['position', 'value']
+# This is the MAIN EXTRACTOR function
+def get_df_topologies_of_bindingsites(positions_df):
+    
+    # FIX THIS! It should be read from the package paths
+    features_data_path = "../../data/features/"
+    
+    # Filenames of the databases with DNA topology data
+    mgw_db_filename = "ecoli-dnashape-mgw.db"
+    helt_db_filename = "ecoli-dnashape-helt.db"
+    prot_db_filename = "ecoli-dnashape-prot.db"
+    roll_db_filename = "ecoli-dnashape-roll.db"
+    #orchid_db_filename = "ecoli-dnashape-orchid"
+    
+    #We load all the DB into memory
+    print("Loading DNA topology databases into memory. This can take a while...")    
+    mgw_data = load_from_topology_db(features_data_path + mgw_db_filename)
+    helt_data = load_from_topology_db(features_data_path + helt_db_filename)
+    prot_data = load_from_topology_db(features_data_path + prot_db_filename)
+    roll_data = load_from_topology_db(features_data_path + roll_db_filename)
+    
+    #df = pd.DataFrame({'x' : [5, 32, 50, 64], 'y' : [13, 40, 58, 72]})
+    # We "currify" the function get_DNA_topology_value to use an specific db
+    f_mgw = partial(get_DNA_topology_value, mgw_data, 'mgw')
+    f_helt = partial(get_DNA_topology_value, helt_data, 'helt')
+    f_prot = partial(get_DNA_topology_value, prot_data, 'prot')
+    f_roll = partial(get_DNA_topology_value, roll_data, 'roll')
+    
+    # We get the topology data
+    df_mgw = positions_df.apply(f_mgw, axis=1)
+    df_helt = positions_df.apply(f_helt, axis=1)
+    df_prot = positions_df.apply(f_prot, axis=1)
+    df_roll = positions_df.apply(f_roll, axis=1)
+    
+    # We concatenate everything
+    res = pd.concat([df_mgw, df_helt, df_prot, df_roll], axis=1)
+    return res
+    
+# Test data frame that simualtes one with START and END positions of predicted Binding sites
+#df = pd.DataFrame({'x' : [5, 32, 50, 64], 'y' : [13, 40, 58, 72]})
+#temp = get_df_topologies_of_bindingsites(df)
+#print(temp)
--- scripts/feature_extraction/tf_bs.py
+++ scripts/feature_extraction/tf_bs.py
@@ -8,35 +8,80 @@ Created on Fri Dec  2 01:59:51 2016
 
 #import paths
 import pandas as pd
-#import numpy as np
+from functools import partial
+#print(regulondb_df)
 
-# FIX THIS! It should be read from the package
-features_data_path = "../../data/features/"
+#fw_strand = regulondb_df[regulondb_df['DNA_strand']=="forward"]
+#rv_strand = regulondb_df[regulondb_df['DNA_strand']=="reverse"]
+#fw_strand.reset_index(drop=True)
+#rv_strand.reset_index(drop=True)
 
-# The name of the file with the RegulonDB data of TFs and their binding site
-regulondb_tfbs_filename = features_data_path + "regulondb-tfbs.txt"
+#mask = (fw_strand['TFBS_start']-position).abs().argsort()
 
-# The name of the columns of the data (check the datafile for more detail)
-column_names =['TF_id', 'TF_name', 'TFBS_id', 'TFBS_start', 'TFBS_end', 'DNA_strand', 'interaction_id', 'unit_regulated','effect','p_name', 'relative_center', 'TFBS_sequence', 'evidence', 'evidence_level']
+#print((fw_strand['TFBS_start']-position).abs().argsort())
+#temp = fw_strand.iloc[mask]
 
-# We read the data file into a Pandas Dataframe
-#regulondb_df = pd.read_csv(regulondb_tfbs_filename, sep='\t', comment='#', header=None, skip_blank_lines=True)
-regulondb_df = pd.read_csv(regulondb_tfbs_filename, sep='\t', comment='#', skip_blank_lines=True, names=column_names)
+#print(regulondb_df['TFBS_start'])
+#print(fw_strand[:2])
+#print(temp)
 
-
-# Retrieves a list of TF that binds in any region between the positions defined by start_position and end_position
-def get_TFs_inregion(start_position, end_position, only_strong = True):
-    global regulondb_df
-    df_filtered = regulondb_df[(regulondb_df['TFBS_start'] >= start_position) & (regulondb_df['TFBS_end'] <= end_position)]
-    if only_strong:
-        df_filtered = df_filtered[df_filtered['evidence_level'] == "Strong"]
-    return df_filtered
-
-def number_of_TFs_around_motif(motif_start, motif_end, neighbourhood_size, only_strong = True):
-    region_start = motif_start - neighbourhood_size
-    region_end = motif_end + neighbourhood_size
-    TFs_df = get_TFs_inregion(region_start, region_end, only_strong)
-    return len(TFs_df)
+def get_distance_to_other_tfs(the_data, a_series):
+    start = a_series[0]
+    end = a_series[1]
+    position = (start + end)/2
+#    position = 58000.0
+    mask = (the_data['TFBS_center_position']-position).abs().argsort()
+    tfs_around_position = the_data.iloc[mask]
+    tfs_upstream_position = tfs_around_position[tfs_around_position['TFBS_center_position'] >= position]
+    tfs_downstream_position = tfs_around_position[tfs_around_position['TFBS_center_position'] < position]
     
-temp = get_TFs_inregion(20000,30000,False)
-print(len(temp))
+    tfs_upstream_position_fw = tfs_upstream_position[tfs_upstream_position['DNA_strand']=="forward"]
+    tfs_upstream_position_rv = tfs_upstream_position[tfs_upstream_position['DNA_strand']=="reverse"]
+    
+    tfs_downstream_position_fw = tfs_downstream_position[tfs_downstream_position['DNA_strand']=="forward"]
+    tfs_downstream_position_rv = tfs_downstream_position[tfs_downstream_position['DNA_strand']=="reverse"]
+    
+    #Some consistency checks
+    assert len(tfs_downstream_position_fw) > 0, "DANGER: No downstream-forward other-TFs binding site!"
+    assert len(tfs_downstream_position_rv) > 0, "DANGER: No downstream-reverse other-TFs binding site!"
+    assert len(tfs_upstream_position_fw) > 0, "DANGER: No upstream-forward other-TFs binding site!"
+    assert len(tfs_upstream_position_rv) > 0, "DANGER: No upstream-reverse other-TFs binding site!"
+    
+    the_list = [tfs_downstream_position_fw, tfs_downstream_position_rv, tfs_upstream_position_fw, tfs_upstream_position_rv]
+    new_list = []
+    for df in the_list:
+        df = df[:1]['TFBS_center_position']-position
+    #    print(df)
+        new_list.append(df)
+    #tfs_downstream_position_fw[:1]['TFBS_center_position']-position
+    res = pd.concat(new_list, axis=0)
+    res = res.abs()
+#    res = data_filtered.iloc[:,1]
+    return pd.Series(res.tolist(), index=['tfs_D_fw', 'tfs_D_rv', 'tfs_U_fw', 'tfs_U_fw'])
+
+    
+def get_df_distances_to_other_tfs(positions_df):
+    # FIX THIS! It should be read from the package
+    features_data_path = "../../data/features/"
+    
+    # The name of the file with the RegulonDB data of TFs and their binding site
+    regulondb_tfbs_filename = features_data_path + "regulondb-tfbs.txt"
+    
+    # Im not using this at the moment
+#    ecoli_genome_highest_position = 4700000.0
+    
+    # The name of the columns of the data (check the datafile for more detail)
+    column_names =['TF_id', 'TF_name', 'TFBS_id', 'TFBS_start', 'TFBS_end', 'DNA_strand', 'interaction_id', 'unit_regulated','effect','p_name', 'relative_center', 'TFBS_sequence', 'evidence', 'evidence_level']
+    
+    # We read the data file into a Pandas Dataframe
+    regulondb_df = pd.read_csv(regulondb_tfbs_filename, sep='\t', comment='#', skip_blank_lines=True, names=column_names)
+    regulondb_df = regulondb_df[regulondb_df['evidence_level'] == "Strong"]
+    #regulondb_df = regulondb_df.sort_values(by='TFBS_start', ascending=True)
+    regulondb_df['TFBS_center_position'] = (regulondb_df['TFBS_start'] + regulondb_df['TFBS_end'])/2
+    
+    distances_getter = partial(get_distance_to_other_tfs, regulondb_df)
+    res = positions_df.apply(distances_getter, axis=1)
+    return res
+
+df = pd.DataFrame({'x' : [5, 32, 50, 64], 'y' : [13, 40, 58, 72]})
+temp = get_df_distances_to_other_tfs(df)
