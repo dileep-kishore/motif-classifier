af237c062ba133e8dfdec92114a58fe1fbc4d70a
Manuel Giménez <manugimenez@gmail.com>
2016-12-03 19:09:20 -0500

    Modified DNA composition extractor to take a dataframe of motif positions as input, and generate also a dataframe as output

--- scripts/feature_extraction/Base_Comp.py
+++ scripts/feature_extraction/Base_Comp.py
@@ -9,6 +9,9 @@ motif. Lm is the location of the left end of the motif. Rm is the right end of o
 motif. DNA is the genomic DNA sequence. W is how far to the left and right of the motif
 we want to explore.'''
 
+from functools import partial
+import pandas as pd
+
 def read_genome(genome_file):
     """Read genome sequence from the genome file"""
     with open(genome_file, 'r') as fid:
@@ -17,37 +20,71 @@ def read_genome(genome_file):
     return sequence
 
 def Base_Comp(Lm,Rm, DNA, W):
+    
+    assert (Lm - W) > 0, "DANGER: Base composition is trying to acces a negative position in the genome"
+    assert (Rm + W) <= len(DNA), "DANGER: Base composition is trying to acces a outofscope position in the genome"
+    
     #Frequency Dictionary
-    Aleft=0
-    Tleft=0
-    Gleft=0
-    Cleft=0
-    Aright=0
-    Tright=0
-    Gright=0
-    Cright=0     
+    Aleft=0.0
+    Tleft=0.0
+    Gleft=0.0
+    Cleft=0.0
+    Aright=0.0
+    Tright=0.0
+    Gright=0.0
+    Cright=0.0   
     Lflank=DNA[Lm-W:Lm]
     Rflank=DNA[Rm:Rm+W]
     #I'
     for i in range(0, len(Lflank)):
         if Lflank[i]=="A":
-            Aleft+=1/W
+            Aleft+=1.0/W
         elif Lflank[i]=="T":
-            Tleft+=1/W
+            Tleft+=1.0/W
         elif Lflank[i]=="G":
-            Gleft+=1/W
+            Gleft+=1.0/W
         elif Lflank[i]=="C":
-            Cleft+=1/W
+            Cleft+=1.0/W
     for i in range(0, len(Rflank)):
         if Rflank[i]=="A":
-            Aright+=1/W
+            Aright+=1.0/W
         elif Rflank[i]=="T":
-            Tright+=1/W
+            Tright+=1.0/W
         elif Rflank[i]=="G":
-            Gright+=1/W
+            Gright+=1.0/W
         elif Rflank[i]=="C":
-            Cright+=1/W
-    return Aleft, Tleft, Gleft, Cleft, Aright, Tright, Gright, Cright
+            Cright+=1.0/W
+    res = [Aleft, Tleft, Gleft, Cleft, Aright, Tright, Gright, Cright]
+    return res
+    
+    
+def adaptor(DNA, W, a_series):
+    Lm = a_series[0]
+    Rm = a_series[1]
+    res = Base_Comp(Lm,Rm, DNA, W)
+    labels = ['compA_d', 'compT_d','compG_d','compC_d','compA_u','compT_u','compG_u','compC_u',]
+    res_series = pd.Series(res, index=labels)
+    return res_series
+    
+    
+def get_df_base_composition(positions_df):
+    # FIX THIS! It should be read from the package paths
+    genome_data_path = "../../data/genome/"
+    # Filenames of the databases with DNA topology data
+    genome_filename = "U00096.2.fa"
+
+    window_size = 25      
+    genome_sequence = read_genome(genome_data_path+genome_filename)
+    
+    # Now we apply the function to every entry in the positions_df
+    base_comp_adpated_partial = partial(adaptor, genome_sequence, window_size)
+    res = positions_df.apply(base_comp_adpated_partial, axis=1)
+    return res
+        
+df = pd.DataFrame({'x' : [50, 320, 500, 640], 'y' : [130, 400, 580, 720]})
+temp = get_df_base_composition(df)
+print(temp)
+    
 # How we can implement this function into a table/list of vectors?
 # FreqMat=np.zeros((Nmotifs+1, 4))
 # given a dataframe of start and end points of each motif, PandaMot, where each column represents
