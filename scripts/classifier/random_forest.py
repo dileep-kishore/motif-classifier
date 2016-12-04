9c8a9a677b0113dba83badbcb21d0bd46b7fde16
Manuel Giménez <manugimenez@gmail.com>
2016-12-04 00:22:19 -0500

    <3

--- scripts/classifier/random_forest.py
+++ scripts/classifier/random_forest.py
@@ -7,26 +7,29 @@ Created on Sat Dec  3 19:48:21 2016
 """
 
 # Random Forest Classification
-import pandas
-from sklearn import cross_validation
+import pandas as pd
+from sklearn.cross_val_score import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
-names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
-dataframe = pandas.read_csv(url, names=names)
-array = dataframe.values
-X = array[:,0:8]
-Y = array[:,8]
+import math
 
-num_folds = 10
-num_instances = len(X)
-seed = 7
-num_trees = 100
-max_features = 3
-
-kfold = cross_validation.KFold(n=num_instances, n_folds=num_folds, random_state=seed)
-model = RandomForestClassifier(n_estimators=num_trees, max_features=max_features)
-results = cross_validation.cross_val_score(model, X, Y, cv=kfold)
-print(results.mean())
-
+def run_classifier(feature_df, labels_df):
+#    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+#    names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+#    dataframe = pandas.read_csv(url, names=names)
+#    array = dataframe.values
+    
+#    num_folds = 10
+#    num_instances = len(feature_df)
+#    seed = 7
+    num_trees = 100
+    max_features = int(math.sqrt(feature_df.shape[1]))
+        
+#    kfold = cross_validation.KFold(n=num_instances, n_folds=num_folds, random_state=seed)
+    model = RandomForestClassifier(n_estimators=num_trees, max_features=max_features)
+#    results = cross_validation.cross_val_score(model, X, Y, cv=kfold)
+#    print(results.mean())
+    
+    scores = cross_val_score(model, feature_df , labels_df, cv=5, scoring='f1')
+    print(scores)
 #MAX FEATURES parameter
 # max_features=sqrt(n_features) for classification tasks (where n_features is the number of features in the data). 
